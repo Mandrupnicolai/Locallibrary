329c80b75a892932233233f5a45767df3483c845
Mandrupnicolai <70701886+Mandrupnicolai@users.noreply.github.com>
2021-06-06 10:55:16 +0200

    First version of application moved into GitHub

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
+
+router.get('/cool', function(req, res, next) {
+  res.send("You're so cool");
+});
+
+module.exports = router;
